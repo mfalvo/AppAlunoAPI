293d7068a0734f3c0e45c04ef47976ad344eae62
Mauricio Falvo <mauricio.falvo@gmail.com>
2024-12-10 15:36:34 -0300

    First commit

--- src/main/java/br/com/spring/login/model/Usuario.java
+++ src/main/java/br/com/spring/login/model/Usuario.java
@@ -27,4 +27,62 @@ public class Usuario implements Serializable {
 	private String nome;
 	private String login;
 	private String senha;
+	/**
+	 * @return the idUsuario
+	 */
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+	/**
+	 * @param idUsuario the idUsuario to set
+	 */
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return this.nome;
+	}
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	/**
+	 * @return the login
+	 */
+	public String getLogin() {
+		return this.login;
+	}
+	/**
+	 * @param login the login to set
+	 */
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	/**
+	 * @return the senha
+	 */
+	public String getSenha() {
+		return this.senha;
+	}
+	/**
+	 * @param senha the senha to set
+	 */
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	/**
+	 * @return the serialversionuid
+	 */
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+	
 }
